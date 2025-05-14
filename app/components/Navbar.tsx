ce038b3b94c4aa20962ffe9db4b695740303a8d3
i9-9 <yvan.vrs@gmail.com>
2025-05-14 10:06:38 -0300

    fix 2

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
-import { usePathname } from 'next/navigation';
 import AboutModal from './AboutModal';
 
 const Navbar = () => {
